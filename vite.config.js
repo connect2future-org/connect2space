26f86b327932ad0dadeeab8758cb8399ca3296bc
Sanjana-402 <lsanjana2110@gmail.com>
2026-07-17 10:35:24 +0530

    Debug API URL

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'sitemap.xml'],
-      manifest: {
-        name: 'Connect2Space',
-        short_name: 'C2S',
-        description: 'Premium Creative Workspace & Podcast Studio',
-        theme_color: '#090414',
-        background_color: '#090414',
-        icons: [
-          {
-            src: '/icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': '/src',
